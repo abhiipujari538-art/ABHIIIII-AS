3cfeaced91f939086fd6fe9ddd1ce526537926b5
abhiipujari538-art <abhiipujari538@gmail.com>
2026-02-22 22:53:45 -0800

    commit2

---
+++ java.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Basic JS</title>
+</head>
+<body>
+
+<h2 id="msg">Hello</h2>
+
+<script>
+  document.getElementById("msg").innerHTML = "Hello JavaScript!";
+</script>
+
+</body>
+</html>
